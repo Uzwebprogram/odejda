78f691197e11dffa803005accb1e907f67360bd5
SarvinozSupersite <Supersite.office@gmail.com>
2023-07-04 12:49:40 +0500

    go push

--- src/entities/news.ts
+++ src/entities/news.ts
@@ -34,7 +34,7 @@ export class NewsEntity {
     @Column({ type: "varchar", length: 2000 })
     @IsString()
     description_ru: string
-    @Column({ type: "varchar", length: 2000 , nullable : true })
+    @Column({ type: "varchar", nullable : true })
     @IsString()
     image: string
     @CreateDateColumn({ type: "timestamp" })
